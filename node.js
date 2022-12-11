8d0880346e5090f4b89d90e015e3f6be92a114e8
Arthur Lu <root@tronnet.net>
2022-12-10 23:05:31 -0800

    fix idnentation in node.js

--- node.js
+++ node.js
@@ -1,32 +1,32 @@
 class Node extends HTMLElement {
-    constructor () { // <link rel="stylesheet" href="style.css" type="text/css">
-        super();
-        let shadowRoot = this.attachShadow({mode: "open"});
+	constructor () { // <link rel="stylesheet" href="style.css" type="text/css">
+		super();
+		let shadowRoot = this.attachShadow({mode: "open"});
 
-        let nodeArticle = document.createElement("article");
-        shadowRoot.append(nodeArticle);
+		let nodeArticle = document.createElement("article");
+		shadowRoot.append(nodeArticle);
 
-        let styleLink = document.createElement("link");
-        styleLink.rel = "stylesheet";
-        styleLink.href = "style.css";
-        styleLink.type = "text/css";
-        shadowRoot.append(styleLink);
-       
-        this.shadowElement = shadowRoot;
-    }
+		let styleLink = document.createElement("link");
+		styleLink.rel = "stylesheet";
+		styleLink.href = "style.css";
+		styleLink.type = "text/css";
+		shadowRoot.append(styleLink);
 
-    set data (data) {
-        let articleElement = this.shadowElement.querySelector("article");
+		this.shadowElement = shadowRoot;
+	}
 
-        let nodeName = document.createElement("h2");
-        nodeName.innerText = data.node;
-        articleElement.append(nodeName);
+	set data (data) {
+		let articleElement = this.shadowElement.querySelector("article");
 
-        let onlineLabel = document.createElement("h3");
-        onlineLabel.innerText = data.status;
-        onlineLabel.style.color = data.status === "online" ? "#00ff00" : "#ff0000";
-        articleElement.append(onlineLabel);
-    }
+		let nodeName = document.createElement("h2");
+		nodeName.innerText = data.node;
+		articleElement.append(nodeName);
+
+		let onlineLabel = document.createElement("h3");
+		onlineLabel.innerText = data.status;
+		onlineLabel.style.color = data.status === "online" ? "#00ff00" : "#ff0000";
+		articleElement.append(onlineLabel);
+	}
 }
 
 customElements.define("node-card", Node);
