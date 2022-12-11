438e220db010b951b8b27b661c3d670e57fec398
Arthur Lu <root@tronnet.net>
2022-12-10 22:49:10 -0800

    implement node-card element, add populating node-cards on init

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+import {requestTicket, setTicket, request} from "./utils.js";
+
 window.addEventListener("DOMContentLoaded", init);
 
 async function init () {
 	let cookie = document.cookie;
-	if(cookie === '') {
+	if (cookie === '') {
 		let username = prompt("username: ");
 		let password = prompt("password: ")
 		let ticket = await requestTicket(username, password);
@@ -10,44 +12,10 @@ async function init () {
 	}
 
 	let nodes = await request("/nodes", "GET", null);
-	console.log(nodes);
-}
-
-async function requestTicket (username, password) {
-	let prms = new URLSearchParams({username: `${username}@pve`, password: password});
-	let response = await fetch("https://pve.tronnet.net/api2/json/access/ticket", {
-		method: "POST",
-		mode: "cors",
-		credentials: "include",
-		headers: {
-			"Content-Type": "application/x-www-form-urlencoded"
-		},
-		body: prms.toString()
-	});
-	let data = await response.json();
-	return data;
-}
-
-async function request (path, method, body) {
-	let prms = new URLSearchParams(body);
-	let response = await fetch(`https://pve.tronnet.net/api2/json${path}`, {
-		method: method,
-		mode: "cors",
-		credentials: "include",
-		headers: {
-			"Content-Type": "application/x-www-form-urlencoded",
-			"Cookie": document.cookie
-		}
-	});
-	if(method == "POST") {
-		response.body = prms.toString();
+	let nodeContainer = document.getElementById("node-container")
+	for (let i = 0; i < nodes.data.length; i++) {
+		let newNode = document.createElement("node-card");
+		newNode.data = nodes.data[i];
+		nodeContainer.append(newNode);
 	}
-	let data = await response.json();
-	return data;
-}
-
-function setTicket (ticket) {
-	let d = new Date();
-	d.setTime(d.getTime() + (2*60*60*1000));
-	document.cookie = `PVEAuthCookie=${ticket}; path=/; expires=${d.toUTCString()}; domain=.tronnet.net`;
 }
---
+++ utils.js
@@ -0,0 +1,38 @@
+export async function requestTicket (username, password) {
+	let prms = new URLSearchParams({username: `${username}@pve`, password: password});
+	let response = await fetch("https://pve.tronnet.net/api2/json/access/ticket", {
+		method: "POST",
+		mode: "cors",
+		credentials: "include",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded"
+		},
+		body: prms.toString()
+	});
+	let data = await response.json();
+	return data;
+}
+
+export function setTicket (ticket) {
+	let d = new Date();
+	d.setTime(d.getTime() + (2*60*60*1000));
+	document.cookie = `PVEAuthCookie=${ticket}; path=/; expires=${d.toUTCString()}; domain=.tronnet.net`;
+}
+
+export async function request (path, method, body) {
+	let prms = new URLSearchParams(body);
+	let response = await fetch(`https://pve.tronnet.net/api2/json${path}`, {
+		method: method,
+		mode: "cors",
+		credentials: "include",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+			"Cookie": document.cookie
+		}
+	});
+	if(method == "POST") {
+		response.body = prms.toString();
+	}
+	let data = await response.json();
+	return data;
+}
