82b739af8e3f3715d0368f47a6a6de96cfb40535
Arthur Lu <root@tronnet.net>
2022-12-10 21:55:34 -0800

    set cookie domain

--- index.js
+++ index.js
@@ -49,5 +49,5 @@ async function request (path, method, body) {
 function setTicket (ticket) {
 	let d = new Date();
 	d.setTime(d.getTime() + (2*60*60*1000));
-	document.cookie = `PVEAuthCookie=${ticket}; path=/; expires=${d.toUTCString()};`;
+	document.cookie = `PVEAuthCookie=${ticket}; path=/; expires=${d.toUTCString()}; domain=.tronnet.net`;
 }
