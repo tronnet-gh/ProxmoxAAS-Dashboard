42aa7655333ac578e2b220cb1f79e0495ee05dbc
Arthur Lu <root@tronnet.net>
2022-12-11 15:24:19 -0800

    sort nodes and instances

--- index.js
+++ index.js
@@ -12,13 +12,17 @@ async function init () {
 	}
 
 	let nodes = await request("/nodes", "GET", null);
+	nodes.data.sort((a, b) => (a.node > b.node) ? 1 : -1);
+
 	let nodeContainer = document.getElementById("node-container")
 	for (let i = 0; i < nodes.data.length; i++) {
 		let newNode = document.createElement("node-card");
 		newNode.data = nodes.data[i];
 		
 		let qemu = await request(`/nodes/${nodes.data[i].node}/qemu`, "GET", null);
+		qemu.data.sort((a, b) => (a.vmid > b.vmid) ? 1 : -1);
 		let lxc = await request(`/nodes/${nodes.data[i].node}/lxc`, "GET", null);
+		lxc.data.sort((a, b) => (a.vmid > b.vmid) ? 1 : -1);
 		newNode.qemu = qemu.data;
 		newNode.lxc = lxc.data;
 
