534c40d7a9a3b6bb40f5d852d80fc3dcf0630f3f
Arthur Lu <root@tronnet.net>
2022-12-19 16:21:24 -0800

    add await to config form request

--- scripts/config.js
+++ scripts/config.js
@@ -11,6 +11,6 @@ async function init () {
 }
 
 async function populateForm (node, type, vmid) {
-	let config = request(`/nodes/${node}/${type}/${vmid}/config`);
+	let config = await request(`/nodes/${node}/${type}/${vmid}/config`);
 	console.log(config);
 }
