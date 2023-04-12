e8908310b62713e37a01db32c02aaae71e8ee5c5
Arthur Lu <root@tronnet.net>
2023-04-12 22:23:40 +0000

    improve styling for dialogs
    Signed-off-by: Arthur Lu <learthurgo@gmail.com>

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -7,13 +7,10 @@ export class Dialog extends HTMLElement {
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 		<link rel="stylesheet" href="css/form.css" type="text/css">
-		<dialog>
-			<p id="prompt" style="text-align: center;"></p>
-			<hr>
-			<form method="dialog" class="input-grid" style="grid-template-columns: auto 1fr;" id="form">
-			</form>
-			<hr>
-			<div class="w3-center">
+		<dialog class="w3-container w3-card-4 w3-border-0">
+			<p class="w3-large" id="prompt" style="text-align: center;"></p>
+			<form method="dialog" class="input-grid" style="grid-template-columns: auto 1fr;" id="form"></form>
+			<div class="w3-center w3-container">
 				<button value="cancel" form="form" class="w3-button w3-margin" style="background-color: #f00;" formnovalidate>CANCEL</button>
 				<button value="confirm" form="form" class="w3-button w3-margin" style="background-color: #0f0;">CONFIRM</button>
 			</div>
