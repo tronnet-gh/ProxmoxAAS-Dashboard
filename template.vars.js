6d8ac3181bd5c08720ae672ef5b8479a4e5396bf
Arthur Lu <learthurgo@gmail.com>
2023-09-15 22:13:21 +0000

    finish renaming, improve search bar and add instance responsiveness

--- template.vars.js
+++ template.vars.js
@@ -1,3 +1,3 @@
-export const API = "https://client.mydomain.example/api"; // the proxmox-aas api
+export const API = "https://dashboard.mydomain.example/api"; // the proxmox-aas api
 export const PVE = "https://pve.mydomain.example"; // the proxmox api
 export const organization = "mydomain"; // org name used in page title and nav bar
