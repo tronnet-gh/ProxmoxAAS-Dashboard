9b7404c8d63ee6ebdb45a5e86450c98b6751419b
Arthur Lu <root@tronnet.net>
2026-05-28 20:05:20 +0000

    update go mod, fix get config to use os.Root

--- app/common/utils.go
+++ app/common/utils.go
@@ -22,15 +22,23 @@ import (
 
 // get config file from configPath
 func GetConfig(configPath string) Config {
-	content, err := os.ReadFile(configPath)
+	root, err := os.OpenRoot(".")
 	if err != nil {
-		log.Fatal("[Error] when opening config file: ", err)
+		log.Fatal("Error when opening root dir: ", err)
 	}
+	defer root.Close()
+
+	content, err := root.ReadFile(configPath)
+	if err != nil {
+		log.Fatal("Error when opening config file: ", err)
+	}
+
 	var config Config
 	err = json.Unmarshal(content, &config)
 	if err != nil {
-		log.Fatal("[Error] during parsing config file: ", err)
+		log.Fatal("Error during parsing config file: ", err)
 	}
+
 	return config
 }
 
